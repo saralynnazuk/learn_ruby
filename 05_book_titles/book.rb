cf4b998efdc2b2caf2ec93ea74dee2b6f86acc5c
Sara Riley <saralynnaz@gmail.com>
2017-05-09 15:26:53 +0100

    05_book_titles - all green

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,11 @@
 class Book
-# write your code here
+attr_reader :title
+
+  def title=(title_string)
+    exceptions = %w[the a an and in of]
+    @title = title_string.split(" ").each_with_index.map { |word, index|
+      exceptions.include?(word) && index != 0 ? word : word.capitalize
+    }.join(" ") #could have all been one line but I chose to break it up for readability
+  end
+
 end
