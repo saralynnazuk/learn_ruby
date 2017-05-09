5056abc281c5e9ae5810032332f96c9f7eb78041
Sara Riley <saralynnaz@gmail.com>
2017-05-09 12:04:33 +0100

    03_simon_says - all green

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
-#write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, num = 2)
+  repeated = (string + " ") * num
+  repeated.rstrip!
+end
+
+def start_of_word(word, n_letters)
+  word[0...n_letters]
+end
+
+def first_word(string)
+   string.partition(" ").first
+end
+
+def titleize(title)
+  small_words = %w[and the over] #list can be extended to include various other small words not to be capitalised
+  title.split(" ").each_with_index.map { |word, index| small_words.include?(word) && index != 0 ? word : word.capitalize }.join(" ")
+end
