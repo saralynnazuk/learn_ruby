4e6b2e3e564726a9619486147ab688cedbbe14a9
Sara Riley <saralynnaz@gmail.com>
2017-05-09 10:43:32 +0100

    02_calculator, inc. extra credit - all green

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(nums)
+  nums.inject(0, :+)
+end
+
+def multiply(*nums)
+  nums.inject(:*)
+end
+
+def power(base, exp)
+  base ** exp
+end
+
+def factorial(num)
+  num == 0 ? 0 : (1..num).inject(:*)
+end
