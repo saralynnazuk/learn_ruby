accd74758580c9c6139767b85306bdd2502bdc3e
Sara Riley <saralynnaz@gmail.com>
2017-05-09 14:03:39 +0100

    04_pig_latin - all green

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,44 @@
-#write your code here
+def translate(string)
+  words = string.downcase.split(" ")
+  vowels = %w[a e i o u]
+  result = []
+
+  words.each_with_index do |word, i|
+    translation = ''
+    qu = false
+    if vowels.include? word[0]
+      translation = word + 'ay'
+      result << translation
+    else
+      word = word.split('')
+      count = 0
+      word.each_with_index do |letter, index|
+        if vowels.include? letter
+          if letter == 'u' && translation[-1] == 'q'
+            qu = true
+            translation = words[i][count + 1..words[i].length] + translation + 'uay'
+            result << translation
+            next
+          end
+          break
+        else
+          if letter == 'q' && word[i + 1] == 'u'
+            qu = true
+            translation = words[i][count + 2..words[i].length] + 'quay'
+            result << translation
+            next
+          else
+            translation += letter
+          end
+          count += 1
+        end
+      end
+
+      if not qu
+        translation = words[i][count..words[i].length] + translation + 'ay'
+        result << translation
+      end
+    end
+  end
+  result.join(' ')
+end
